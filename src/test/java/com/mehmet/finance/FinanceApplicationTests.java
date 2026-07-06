97812120972fdfcd7f71464f04921000df20141f
mehmet <mehmetvia49@gmail.com>
2026-07-06 19:21:36 +0300

    düzeltmeler

--- src/test/java/com/mehmet/finance/FinanceApplicationTests.java
+++ src/test/java/com/mehmet/finance/FinanceApplicationTests.java
@@ -1,13 +1,23 @@
 package com.mehmet.finance;
 
+import com.mehmet.finance.entities.User;
+import com.mehmet.finance.services.UserService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+@SpringBootTest(classes = FinanceApplication.class)
+@RequiredArgsConstructor
 class FinanceApplicationTests {
-
+    private final UserService userService;
     @Test
-    void contextLoads() {
+    public void testGetUserById() {
+        User user = userService.getUserById(1L);
+        assertNotNull(user);
     }
+
+
+
 
 }
